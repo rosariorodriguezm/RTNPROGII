e091280570f7db2b0e8a84d57cc1966964c1df0d
rosariorodriguezm <rrodriguezmontenegro@udesa.edu.ar>
2020-05-15 16:45:10 -0300

    primer js

---
+++ ProyectoRoTi/public/javascripts/alAire.js
@@ -0,0 +1,38 @@
+window.addEventListener('load',function(){
+    //
+  var api_key = "60be6317b0012697cb25d6ce427de920"
+  fetch("https://api.themoviedb.org/3/tv/airing_today?api_key="+api_key+"&language=es-AR&page=1")
+  .then(function(response) {
+   return response.json()
+  })
+  .then(function(information) {
+    var urlFija = "https://image.tmdb.org/t/p/original"
+    var titulo = ""
+    var posterURL = ""
+    var serie = ""
+    var ul = document.querySelector('ul#AlAire')
+    for (var i = 0; i < information.results.length; i++) {
+      titulo = information.results[i].name
+      var id = information.results[i].id
+      console.log(id);
+      console.log(information.results[i].poster_path);
+      posterURL = urlFija + information.results[i].poster_path
+  
+      serie = '<li  class="uk-animation-toggle" tabindex="0" >'
+      serie += '<a href="detalle.html?id=' + id + '">'
+      serie +=   '<img class="uk-animation-fade" src="'+posterURL+'" alt="">'
+      serie +=   '<div class="uk-position-center uk-panel"><h2 class="">'+'</h2></div>'
+      serie += '</a>'
+      serie += '</li>'
+  
+      console.log(serie);
+      ul.innerHTML += serie
+    }
+  })
+  .catch(function(error) {
+   console.log("Error: " + error);
+  })
+  
+  
+  
+  })

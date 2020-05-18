38c89a8dc8e193e4d540a1e0c7fb8949c197b039
rosariorodriguezm <rrodriguezmontenegro@udesa.edu.ar>
2020-05-18 12:18:51 -0300

    rutas del resto de las pags

--- ProyectoRoTi/routes/pages.js
+++ ProyectoRoTi/routes/pages.js
@@ -5,6 +5,18 @@ const controller = require('../controllers/pagesController')
 
 router.get('/', controller.home);
 
+router.get('/detalle', controller.detalle)
 
+router.get('/favoritos', controller.favoritos)
+
+router.get('/generos', controller.generos)
+
+router.get('/perfil', controller.perfil)
+
+router.get('/resultadosbusq', controller.resultadosBusq)
+
+router.get('/series-por-genero', controller.seriesPorGenero)
+
+router.get('/series', controller.series)
 
 module.exports = router;

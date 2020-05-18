a3eec6db4999666e27085119451c6af3d6053181
rosariorodriguezm <rrodriguezmontenegro@udesa.edu.ar>
2020-05-18 12:20:44 -0300

    agregue ; desp de route.get

--- ProyectoRoTi/routes/pages.js
+++ ProyectoRoTi/routes/pages.js
@@ -5,18 +5,18 @@ const controller = require('../controllers/pagesController')
 
 router.get('/', controller.home);
 
-router.get('/detalle', controller.detalle)
+router.get('/detalle', controller.detalle);
 
-router.get('/favoritos', controller.favoritos)
+router.get('/favoritos', controller.favoritos);
 
-router.get('/generos', controller.generos)
+router.get('/generos', controller.generos);
 
-router.get('/perfil', controller.perfil)
+router.get('/perfil', controller.perfil);
 
-router.get('/resultadosbusq', controller.resultadosBusq)
+router.get('/resultadosbusq', controller.resultadosBusq);
 
-router.get('/series-por-genero', controller.seriesPorGenero)
+router.get('/series-por-genero', controller.seriesPorGenero);
 
-router.get('/series', controller.series)
+router.get('/series', controller.series);
 
 module.exports = router;

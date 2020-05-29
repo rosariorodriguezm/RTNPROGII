cb801c3df138324d44eeb474cb7194aa6c0b69aa
rosariorodriguezm <rrodriguezmontenegro@udesa.edu.ar>
2020-05-29 12:20:35 -0300

    listado de usuarios

--- ProyectoRoTi/controllers/reseniasController.js
+++ ProyectoRoTi/controllers/reseniasController.js
@@ -5,7 +5,6 @@ module.exports = {
       DB.Resenas //Reseñas es el primer parametro que pase en mi modelo
         .findAll() //me traigo todas las reseñas
         .then(resenas => { //cuando este listo, quiero q me devuelva todas las reseñas y ahi la vista de esa pag
-         // res.send(resenas)
           return res.render('todas_resenas', {
             listaResenas: resenas
             //la vista entiende a todas las reseñas como listaReseñas

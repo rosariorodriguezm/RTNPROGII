dbc57db7b74320d18e1e14ba45cd0f62c53e187d
rosariorodriguezm <rrodriguezmontenegro@udesa.edu.ar>
2020-05-31 11:19:34 -0300

    probe si funcionaba, pero no :)

--- ProyectoRoTi/public/javascripts/buscar-usuario.js
+++ ProyectoRoTi/public/javascripts/buscar-usuario.js
@@ -2,26 +2,11 @@
 var elFormulario = document.querySelector('.perfil_usuarios');
 var campoNombre = document.querySelector('#nombre_buscado');
 var campoMail = document.querySelector('#mail_buscado');
-var campoSeguidoresSerie = document.querySelector('#serie_seguida');
 
 elFormulario.onsubmit = function (event) {
-  if (campoNombre.value == "" || campoMail.value == "") {
+if (campoNombre.value == "" || campoMail.value == "") {
     event.preventDefault();
     alert("Llenar el campo Nombre o Mail");
   }
 }
 
- // Capturamos lo que vino en la query string
- let searchParams = new URLSearchParams(window.location.search);
- let name = searchParams.get('name')
- // var withoutG = searchParams.get('withoutGenres')
- let email = searchParams.get('mail')
- let seguidorSerie = searchParams.get('seguidor')
-
- botonBuscar = document.getElementById('#search_user')
-
- // Si vinieron cosas en la query string me armo la URL
- if (name != null && email != null) {
-    botonBuscar.innerHTML += `
-    <a href="/pages/perfil-usuario?nombre=${name}&mail=${email}"></a>
-    `}

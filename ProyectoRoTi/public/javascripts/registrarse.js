e591d08a7ca452372f080ff9f673323b7d82b65a
Tiziana <tizimb@gmail.com>
2020-06-05 23:46:05 -0300

    genero favorito con la api :)

--- ProyectoRoTi/public/javascripts/registrarse.js
+++ ProyectoRoTi/public/javascripts/registrarse.js
@@ -1,29 +1,70 @@
+window.onload = function() {
+    var listaGeneros = document.querySelector('#dropdown');
+    
+    var api_key = "60be6317b0012697cb25d6ce427de920"
+    fetch("https://api.themoviedb.org/3/genre/tv/list?api_key="+api_key+"&page=1")
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(informacion){
+        var generos = informacion.genres;
+        for (var i = 0; i< generos.length; i++){
+            listaGeneros.innerHTML += 
+           "<option value='"+generos[i].name+"'>"+generos[i].name+"</option>" 
+        
+        }
+    })
+}
 
-////////////////////////GUARDO NUEVO USUARIO//////////////////////////
 
-$(document).ready(function(){
-    $('#regis').click(function(){
-        /*Captura de datos escrito en los inputs*/
-        var nombreNuevo = document.getElementById("nuevoNom").value;//2do --> id input
-        var emailNuevo = document.getElementById("nuevoEmail").value;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////GUARDO NUEVO USUARIO////////////////////////// PROG I
+
+//$(document).ready(function(){
+  //  $('#regis').click(function(){
+    //    /*Captura de datos escrito en los inputs*/
+      //  var nombreNuevo = document.getElementById("nuevoNom").value;//2do --> id input
+        //var emailNuevo = document.getElementById("nuevoEmail").value;
 
 
 
 
 
         /*Guardando los datos en el LocalStorage*/
-        localStorage.setItem("nuevoNom", nombreNuevo); //2do --> variable
-        localStorage.setItem("nuevoEmail", emailNuevo);
+  //      localStorage.setItem("nuevoNom", nombreNuevo); //2do --> variable
+//        localStorage.setItem("nuevoEmail", emailNuevo);
 
 
 
 
         /*Limpiando los campos o inputs*/
-        document.getElementById("nuevoNom").value = "";
-        document.getElementById("nuevoEmail").value = "";
+   //     document.getElementById("nuevoNom").value = "";
+     //   document.getElementById("nuevoEmail").value = "";
 
 
 
-    });
-});
+   // });
+//});
+
+
+
 

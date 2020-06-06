bca6bde43fbfed69fbcf3b3d9097bbccad944112
rosariorodriguezm <rrodriguezmontenegro@udesa.edu.ar>
2020-06-06 19:36:31 -0300

    lo volvi a poner

--- ProyectoRoTi/controllers/usuariosController.js
+++ ProyectoRoTi/controllers/usuariosController.js
@@ -46,6 +46,7 @@ module.exports = {
                 //si el usuario esta undefined, es decir, no coincide, me devuelve al form de inicio de sesion    
                 
             } else {
+                req.session.usuario = usuario;
                 res.redirect('/usuarios/miPerfil/'+ usuario.id)
             //si el usuario existe, me redirige al listado de resenias de ese usuario --> '/usuarios/resenias/:id'
               }
